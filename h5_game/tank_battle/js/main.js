015fbd8829bf3afd9f5beebf5182d110e5e9c2b9
ahri su <douhl@foxmail.com>
2025-07-22 22:05:25 +0800

    坦克大战

--- h5_game/tank_battle/js/main.js
+++ h5_game/tank_battle/js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
 	initObject();
 	
 	setInterval(gameLoop,20);
-	document.body.style.transform = 'scale(0.8)'; // 将网页缩放至80%
+	document.body.style.transform = 'scale(0.75)'; // 将网页缩放至80%
 	//setViewportSize(); // 初始化时也调用一次
 });
 
