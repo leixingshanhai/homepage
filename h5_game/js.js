80e3ad8693bf9b895224782d022b012e99364428
ahri su <douhl@foxmail.com>
2025-07-25 19:36:43 +0800

    个人主页

--- h5_game/js.js
+++ h5_game/js.js
@@ -27,6 +27,41 @@
   });
   
   function cardClick(type){
-	  var url = type + '/index.html';
-	  window.open(url,'h5_game_page');
+	  if(type == 'tank_battle'){
+		  if(isMobileDevice()){
+			alert("请使用电脑端进行游玩~");
+		  }else{
+			var url = type + '/index.html';
+		    window.open(url,'h5_game_page');
+		  }
+	  }else if(type == 'flying_shooting_game'){
+		  if(isMobileDevice()){
+			var url = type + '/index.html';
+		    window.open(url,'h5_game_page');
+		  }else{
+			alert("请使用移动端进行游玩~");
+		  }
+	  }else{
+	    var url = type + '/index.html';
+	    window.open(url,'h5_game_page');
+	  }
+  }
+  
+  function isMobileDevice() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+ 
+    // 这些表达式用来检查设备是否为移动设备
+    var isAndroid = /android/i.test(userAgent);
+    var isiOS = /iphone|ipad|ipod/i.test(userAgent);
+    var isOpera = /opera mini/i.test(userAgent);
+    var isWindowsPhone = /windows phone/i.test(userAgent);
+    var isBlackBerry = /blackberry/i.test(userAgent);
+    var isMobile = /mobile/i.test(userAgent); // 基本移动设备
+ 
+    // 如果满足任何移动设备的条件，则为移动设备
+    if ((isAndroid || isiOS || isOpera || isWindowsPhone || isBlackBerry || isMobile)) {
+        return true;
+    } else {
+        return false;
+    }
   }
